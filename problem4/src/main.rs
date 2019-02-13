80fba387687b8b3976d4c7c542bafbae1539d8d2
Thomas Avery <tavery2@binghamton.edu>
2019-02-13 10:10:57 -0500

    Problem 4 complete, neat rust stuff to get palindrome

--- problem4/src/main.rs
+++ problem4/src/main.rs
@@ -1,4 +1,28 @@
+fn is_palindrome(total: i32) -> bool
+{
+    let int_string = total.to_string();
+    let half = int_string.len()/2;
+    let pal = int_string.bytes().take(half).eq(int_string.bytes().rev().take(half));
+    return pal
+}
+
 fn main() 
 {
-    
+    let mut max = 0;
+    for num1 in (100..1000).rev()
+    {
+        for num2 in (100..1000).rev()
+        {
+            let total = num1*num2;
+            let ret = is_palindrome(total);
+            if ret == true
+            {
+                if total > max
+                {
+                    max = total;
+                }   
+            }
+        }
+    }
+    println!("{}", max);
 }
